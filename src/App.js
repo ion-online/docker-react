b53a3b9ee381594bfb3a0ef4a3452a20ce1df86c
Ion <ion.ermurachi.online@gmail.com>
2020-10-22 19:27:36 +0000

    bla bla

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>YAHOOOOOOO</h1>
+        <h2>YAHOOOOOOO</h2>
+        <h3>YAHOOOOOOO</h3>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
